443fdcfa03442744c4db82314e00ae79d53b85d8
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2024-02-02 13:50:36 +0100

    pliki bazowe dodane
    do app.js trzeba dorobic folder z zainstalowanym expressem, do vsc trzeba dodac newtonsoft.json a mainapplication nalezy nadpisac zeby poprawnie pobieral dane z niezabezpieczonego adresu http (serwer musi chodzic caly czas kiedy uzywamy aplikacji)

---
+++ MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace mobilnaZnodeJsBadowski4c;
+
+public partial class MainPage : ContentPage
+{
+    List<Stacja> stacje = new List<Stacja>();
+    public ObservableCollection<Stacja> listaStacji { get; set; }
+	public class Stacja
+	{
+        public string id_stacji { get; set; }
+        public string stacja { get; set; }
+        public string data_pomiaru { get; set; }
+        public string godzina_pomiaru { get; set; }
+        public string temperatura { get; set; }
+        public string predkosc_wiatru { get; set; }
+        public string kierunek_wiatru { get; set; }
+        public string wilgotnosc_wzgledna { get; set; }
+        public string suma_opadu { get; set; }
+        public string cisnienie { get; set; }
+    }
+    public async void GetPogoda()
+    {
+        HttpClient _client = new HttpClient();
+        HttpResponseMessage response = await _client.GetAsync("http://10.0.2.2:3000/dane");
+        var contents = await response.Content.ReadAsStringAsync();
+        stacje = JsonConvert.DeserializeObject<List<Stacja>>(contents);
+        Dane_label.Text = "Dane z serwisu zostały pobrane";
+    }
+    private void onEntryCompleted(object sender, EventArgs e)
+    {
+        var jest = false;
+        foreach(var entry in stacje)
+        {
+            if(entry.id_stacji == Identyfikator.Text)
+            {
+                DisplayAlert("Informacje", "Stacja: " + entry.stacja + "\nTemperatura: " + entry.temperatura + "\nCisnienie: " + entry.cisnienie, "OK");
+                jest = true;
+            }
+        }
+        if (!jest)
+        {
+            DisplayAlert("Informacja", "Nie odnaleziono stacji o id = " + Identyfikator.Text, "OK");
+        }
+    }
+
+    private void wybrany(object sender, SelectedItemChangedEventArgs e)
+    {
+        string idStacji = (e.SelectedItem as Stacja).id_stacji;
+        string nazwa = (e.SelectedItem as Stacja).stacja;
+        DisplayAlert("Informacje", "Stacja: " + nazwa + "\nId stacji: " + idStacji + "\nData pomiaru: " + (e.SelectedItem as Stacja).data_pomiaru, "OK");
+    }
+
+    private void dodaj(object sender, EventArgs e)
+    {
+        for (int i = 0; i < stacje.Count; i++)
+        {
+            listaStacji.Add(new Stacja
+            {
+                id_stacji = stacje[i].id_stacji,
+                stacja = stacje[i].stacja,
+                data_pomiaru = stacje[i].data_pomiaru,
+                godzina_pomiaru = stacje[i].godzina_pomiaru,
+                temperatura = stacje[i].temperatura,
+                predkosc_wiatru = stacje[i].predkosc_wiatru,
+                kierunek_wiatru = stacje[i].kierunek_wiatru,
+                wilgotnosc_wzgledna = stacje[i].wilgotnosc_wzgledna,
+                suma_opadu = stacje[i].suma_opadu,
+                cisnienie = stacje[i].cisnienie
+            });
+        }
+    }
+
+	public MainPage()
+	{
+		InitializeComponent();
+         
+        GetPogoda();
+
+        listaStacji = new ObservableCollection<Stacja> { };
+        BindingContext = this;
+	}
+
+}
+
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+app.use(express.json())
+function getData() {
+    fetch('https://danepubliczne.imgw.pl/api/data/synop')
+    .then(response => response.json())
+    .then(jsonData => fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => err && console.error(err)))
+}
+/*function getLocations() { // to chyba bez sensu
+    fetch('https://astronomia.zagan.pl/art/wspolrzedne.html')
+    .then(response => response.set('charset=utf-8'))
+    .then(utfresponse => utfresponse.text())
+    .then(stringData => console.log(stringData))
+    //.then(stringData => fs.writeFile("lokacje.txt", stringData, (err) => err && console.error(err)))
+}*/
+//getLocations()
+getData()
+setInterval(getData, 30000)
+app.get('/dane', (req, res) => {
+    fs.readFile("./data.json", "utf8", (error, data) => {
+        if(error) {
+            console.log(error)
+            return;
+        }
+        res.send(JSON.parse(data))
+    })
+})
+app.get("/dane/id", (req, res) => {
+    stacjaId = req.query.id
+    fs.readFile("./data.json", "utf8", (error, data) => {
+        if(error) {
+            console.log(error)
+            return;
+        }
+        var json = new Array()
+        json = JSON.parse(data)
+
+        res.send(json[stacjaId])
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`Server is Listening on 3000`)
+})
